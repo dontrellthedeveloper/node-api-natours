53033046f3aada687e9580088acaca2816223627
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-12-22 02:01:20 -0800

    setup ENV variables

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
 const app = require('./app');
 
+
+// console.log(process.env);
+
 /* =============== Express Server ================ */
-const port = 9000;
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`)
 });
