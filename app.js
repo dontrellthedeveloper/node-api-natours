0fd54711b4cb7dfa88671bd8903ec3ed73472457
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-12-21 16:17:22 -0800

    Handling patch requests

--- app.js
+++ app.js
@@ -82,5 +82,25 @@ app.post('/api/v1/tours', (req, res) => {
     });
 });
 
+/* =============================================
+|  |  |  |  |  PATCH request
+================================================ */
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here ...>'
+        }
+    })
+});
+
+
 
 
