92dc23aab24b68d372011a1608e8d4fbd7f8a109
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-12-22 22:28:02 -0800

    connected mongo database to application

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
 const app = require('./app');
 
+dotenv.config({path: './config.env'});
 
-// console.log(process.env);
+
+/* =============== Mongo Database ================ */
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD);
+
+mongoose
+    .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then(() => console.log('DB connection successful!'));
+
 
 /* =============== Express Server ================ */
 const port = process.env.PORT || 9000;
