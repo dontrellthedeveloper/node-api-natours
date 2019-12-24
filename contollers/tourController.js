8bf00e9c9645e50637018c127f7e488bbc734185
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-12-24 11:48:02 -0800

    Added way to read and update Tour model. Tested with postmen.

--- contollers/tourController.js
+++ contollers/tourController.js
@@ -5,31 +5,46 @@ const Tour = require('./../models/tourModel');
 
 /* ================ Get All Tours ================= */
 
-exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        // results: tours.length,
-        // data: {
-        //     tours
-        // }
-    })
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
+
 };
 
 /* ================ Get One Tour ================= */
 
-exports.getTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-    // const tour = tours.find(el => el.id === id);
-    //
-    // res.status(200).json({
-    //     status: 'success',
-    //     data: {
-    //         tour
-    //     }
-    // });
+exports.getTour = async (req, res) => {
+    try {
+       const tour = await Tour.findById(req.params.id);
+       // Tour.findOne({ _id: req.params.id })
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
+
+
 };
 
 /* ================= Create Tour ================== */
@@ -37,6 +52,8 @@ exports.getTour = (req, res) => {
 exports.createTour = async (req, res) => {
 
     try {
+        // const newTour = new Tour({})
+        // newTour.save()
         const newTour = await Tour.create(req.body);
 
         res.status(201).json({
@@ -55,14 +72,26 @@ exports.createTour = async (req, res) => {
 
 /* ================= Update Tour ================== */
 
-exports.updateTour = (req, res) => {
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
+    
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here ...>'
-        }
-    })
 };
 
 /* ================= Delete Tour ================== */
