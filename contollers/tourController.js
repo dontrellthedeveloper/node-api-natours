0691424a8b7140a652c252d6873fe414bd30d834
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-12-24 19:46:07 -0800

    Implemented sorting in Tours request

--- contollers/tourController.js
+++ contollers/tourController.js
@@ -8,20 +8,29 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
     try {
         console.log(req.query);
-        // Build Query
-        // 1) Filtering
+
+        // BUILD QUERY
+
+        // 1A) Filtering
         const queryObj = {...req.query};
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach(el => delete  queryObj[el]);
 
-
-        // 2) Advanced Filtering
+        // 1B) Advanced Filtering
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
 
+        let query = Tour.find(JSON.parse(queryStr));
 
-        const query = Tour.find(JSON.parse(queryStr));
+        // 2) Sorting
+        if(req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy)
+            // sort('price ratingsAverage')
+        } else {
+            query.sort('-createdAt');
+        }
+
 
         // Execute Query
         const tours = await query;
